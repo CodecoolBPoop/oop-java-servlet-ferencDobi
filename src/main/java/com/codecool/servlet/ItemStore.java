0f26ab54f0c59b81154e1fc8bf0200fdf8d0e926
ferencDobi <ferencdbi@gmail.com>
2018-08-16 14:44:58 +0200

    itemstore implement

---
+++ src/main/java/com/codecool/servlet/ItemStore.java
@@ -0,0 +1,32 @@
+package com.codecool.servlet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemStore {
+    private static List<Item> items = new ArrayList<>();
+
+    static void add(Item item) {
+        items.add(item);
+    }
+
+    static void remove(Item item) {
+        items.remove(item);
+    }
+
+    static Item get(int index) {
+        return items.get(index);
+    }
+
+    static int size() {
+        return items.size();
+    }
+
+    /*
+    contains a static List (ArrayList or LinkedList) of Items.
+    It should provide static functions as well, like add and remove.
+    The business logic is not that important, adding multiple Items with the same id is valid use case.
+
+    Pro version: store it in a session instead of a static list!
+     */
+}
